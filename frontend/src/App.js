12c66a847ac46af63ede6eada1d34bda70641eac
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-06-03 18:06:06 +0900

    フロントからinsertとupdateができるようになりました。

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Auth } from "@polybase/auth";
 import { Polybase } from "@polybase/client";
 import { AuthProvider, PolybaseProvider, useAuth, useDocument, usePolybase } from "@polybase/react";
+import { useState } from "react";
 import './App.css';
 import Spinner from './common/Spinner';
 import logo from './logo.svg';
@@ -18,11 +19,40 @@ const auth = new Auth();
  * @returns 
  */
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   // Polybaseを使うための準備
   const polybase = usePolybase();
   const { auth, state } = useAuth();
   // DBから情報を取得する。
   const { data, error, loading } = useDocument(polybase.collection("City"));
+
+  /**
+   * insert new record
+   */
+  const insert = async() => {
+    try {
+      setIsLoading(true);
+      await polybase.collection("City").create(["new-york2", "New York2"]); 
+      setIsLoading(false);
+    } catch(err) {
+      console.log("err:", err);
+      setIsLoading(false);
+    }
+  } 
+
+  /**
+   * update
+   */
+  const update = async() => {
+    try {
+      setIsLoading(true);
+      await polybase.collection("City").record("new-york2").call("setCountry", ["USA22"]);
+      setIsLoading(false);
+    } catch(err) {
+      console.log("err:", err);
+      setIsLoading(false);
+    }
+  }
 
   /**
    * DBの取得結果を表形式で出力
@@ -41,24 +71,32 @@ function App() {
           <Spinner/>
         ) : (
           <>
-            {(state === null) ? (
-              <button onClick={() => auth.signIn()}>Sign In</button>
-            ) : (
+            {isLoading ? (
+              <Spinner/>
+            ) :(
               <>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <button onClick={() => auth.signOut()}>Sign Out</button>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-                {listItems}
+                {(state === null) ? (
+                  <button onClick={() => auth.signIn()}>Sign In</button>
+                ) : (
+                  <>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <button onClick={() => auth.signOut()}>Sign Out</button>
+                    <button onClick={() => insert()}>insert</button>
+                    <button onClick={() => update()}>update</button>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn React
+                    </a>
+                    {listItems}
+                  </>
+                )}
               </>
             )}
           </>
